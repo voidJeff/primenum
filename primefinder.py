819d56114fbb7a1e113dd69a7453976fa7dd1388
voidJeff <qy120838@126.com>
2015-10-01 15:21:14 -0400

    Updated from Brython Server: 10/1/2015 3:21:13 PM

--- primefinder.py
+++ primefinder.py
@@ -1,10 +1,14 @@
 lim = int(input("Set a limit for finding prime numbers "))
 num = list(range(2, lim+1))
-i = 2
-a = 1
+n = 0
+i = num[n]
+a = 2
 print(2, end="")
 while i**2 <= lim:
     while i*a <= lim:
         num.remove(i*a)
         a += 1
+    n += 1
+    a = i
+print(num)
     
