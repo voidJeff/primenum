6b9c93eff01622207ce067ae910be26d54446a1a
voidJeff <qy120838@126.com>
2015-10-01 15:15:44 -0400

    Updated from Brython Server: 10/1/2015 3:15:44 PM

--- primefinder.py
+++ primefinder.py
@@ -2,19 +2,7 @@ lim = int(input("Set a limit for finding prime numbers "))
 i = 2
 a = 1
 print(2, end="")
-while :
-    num += 1
-    boov = 0
-    if num%2 != 0:                      #if 2 does not divide it
-        boov = 0
-        while factor < rangenum:
-            if num%factor != 0:         #if the current factor does not divide it
-                boov += 0
-            else:
-                boov -= 1
-            factor += 1                 #change the testing factor
-    else:
-        boov -= 1
-    if boov == 0:
-        print(num)
-    rangenum -= 1
+while i**2 <= lim:
+    while i*a <= lim:
+        h
+    
