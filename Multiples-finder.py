1e8206ce2bad6e740c8a44555a62180217ad4fd5
voidJeff <qy120838@126.com>
2015-11-09 14:47:57 -0500

    Updated from Brython Server: 11/9/2015 2:47:57 PM

--- Multiples-finder.py
+++ Multiples-finder.py
@@ -1,1 +1,10 @@
-
+i = 2
+num = int(input("Enter the number: ")
+multiples = []
+while i < num / 2:
+    if num % i == 0:
+        multiples.append(i)
+        num = num / i
+    else:
+        i += 1
+print(list(multiples))
