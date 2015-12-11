caff09a885f1a706bcbb1f75d9c51be94d716952
voidJeff <qy120838@126.com>
2015-12-11 14:48:07 -0500

    Updated from Brython Server: December 11, 2015 2:48:07 PM EST

--- equation solver.py
+++ equation solver.py
@@ -1,7 +1,9 @@
 i = 2
 a = 0
+b = 1
 num = int(input("Enter the number: "))
 multiples = []
+factors = []
 while i <= num:
     if num % i == 0:
         multiples.append(i)
@@ -10,5 +12,12 @@ while i <= num:
         i += 1
 while a <= len(multiples):
     repeat_ = multiples.count(multiples[a])
+    if repeat_%2 == 0:
+        b = b * repeat_ / 2
+    else:
+        b = b * (repeat_ - 1)/2
+        factors.append(repeat_)
+    
+    a = a + repeat_ - 1
 print(list(multiples))
 print(repeat_)
