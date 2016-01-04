133b8f475cacb9c53ba564f2a1733cf484725c99
voidJeff <qy120838@126.com>
2016-01-04 14:34:40 -0500

    Updated from Brython Server: January 4, 2016 2:34:40 PM EST

--- Multiples-finder.py
+++ Multiples-finder.py
@@ -1,3 +1,4 @@
+'''
 i = 2
 num = int(input("Enter the number: "))
 multiples = []
@@ -8,3 +9,26 @@ while i <= num:
     else:
         i += 1
 print(list(multiples))
+'''
+
+multiples = []
+lim = int(input("Enter the number: "))
+num = list(range(2, lim+1))
+n = 0
+a = 2
+while num[n]**2 <= lim:
+    while num[n]*a <= lim:
+        if num.count(num[n]*a) != 0:
+            num.remove(num[n]*a)
+        a += 1
+    n += 1
+    a = num[n]
+b = len(num)
+while b >= 0:
+    if lim % num[-b] == 0:
+        multiples.append(num[-b])
+        lim = lim/num[-b]
+    else:
+        b -= 1
+print(list(multiples))        
+    
